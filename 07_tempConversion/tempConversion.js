b26532f59d1735859e827fe1baa1a4225ee7aee4
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 14:38:08 +0530

    temp conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+let result=(temp-32)*5/9;
+return Math.round(result*10) / 10 ;
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+let result = 9*temp/5 + 32;
+return Math.round(result*10) / 10 ;
 };
 
 // Do not edit below this line

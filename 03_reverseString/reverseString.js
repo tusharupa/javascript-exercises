120523998ec7cd3ee6c243c00c3be25fc8420d91
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 11:01:21 +0530

    reverse string done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const len=word.length;
+    let result='';
+for(let i=0;i<len;i++)
+{
+result=result+word.charAt(len-i-1);
+}
+return result;
 };
 
 // Do not edit below this line

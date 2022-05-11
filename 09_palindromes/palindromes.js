8b9177415aea5eca4999b0af66dda3ed23641b9e
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 15:33:10 +0530

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (palin) {
+    newString=palin.toLowerCase().replace(/[^a-z]/g, "");
+    anotherString = newString.split("").reverse().join("");
 
+ if (anotherString==newString)
+ {
+     return true;
+ }
+ else
+ return false;
 };
 
 // Do not edit below this line

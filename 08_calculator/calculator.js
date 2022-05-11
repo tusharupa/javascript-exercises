3e0debd6a280591941843ed2672ccb5008b23cff
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 15:00:50 +0530

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arraySum) {
+  let add=0;
+	for(let i=0;i<arraySum.length;i++)
+  {
+    add+=arraySum[i];
+  }
+  return add;
 };
 
-const multiply = function() {
-
+const multiply = function(multi) {
+let result=1;
+for(let i=0;i<multi.length;i++)
+{
+result=result*multi[i];
+}
+return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let result = 1;
+  if(num==0)
+  return 1;
+  else
+  {
+  while(num>0)
+  {
+    result = result * num;
+    num--;
+  }
+  return result;
+}
 	
 };
 

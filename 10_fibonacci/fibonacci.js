4b62d2539c2c0e9ae4fbc1120db3b0f03eca78ee
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 15:50:01 +0530

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if(n<0)
+    return 'OOPS';
+if(n==0)
+return 0;
+let a=0;
+let b=1;
+
+for(let i=1;i<n;i++)
+{
+    const temp=b;
+    b=a+b;
+    a=temp;
+}
+return b;
 };
 
 // Do not edit below this line

841bc1f33aae0a00b286ec7c83ecd87ed8630b93
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 14:05:30 +0530

    sum all done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(initialValue,finalValue) {
+let sum=0;
+if(initialValue < 0 || finalValue < 0 )
+return 'ERROR';
+else{
+if(Number.isInteger(initialValue) && Number.isInteger(finalValue))
+{
+    if(initialValue>finalValue)
+    {
+        let temp=initialValue;
+        initialValue=finalValue;
+        finalValue=temp;
+    }
+while(initialValue<=finalValue)
+{
+    sum+=initialValue;
+    initialValue++;
+}
+return sum;
+}
+else
+return 'ERROR';
+};
 };
 
 // Do not edit below this line

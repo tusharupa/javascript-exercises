e4defa1434d5f2bec34b73c4f2c4332cd16b37ef
tushar upadhyay <tusharupa99@gmail.com>
2022-05-11 10:34:53 +0530

    first two exercises done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(String,count) {
+    let result='';
+    if(count>=0)
+    {
+for(let i=0;i<count;i++)
+{
+    result +=String;
+}
+return result.trim();
+    }
+    else
+    return 'ERROR';
 };
 
 // Do not edit below this line
